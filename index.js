9056a2268faddd73b3177bfb63335f70e82ad807
Adam Barrell <a.j.barrell@gmail.com>
2015-04-18 16:35:11 +0100

    Implemented correct UID reading logic.

--- index.js
+++ index.js
@@ -1,66 +1,24 @@
 var HID = require('node-hid');
 
-console.log(HID.devices());
+var STOP_BYTE = 40;
+
+var deviceId = 'USB_4242_e131_14100000',
+    cardId = '';
+
+function reader(data) {
+    var dataNibble = data[1];
+
+    if ( dataNibble === STOP_BYTE ) {
+        console.log('Card ID:', cardId);
+        cardId = '';
+    } else if ( dataNibble !== 0 ) {
+        cardId += (++dataNibble) % 10;
+    }
+}
 
 try {
-    var scannerId = 'USB_4242_e131_14100000',
-        scanner = new HID.HID(scannerId);
-
+    var scanner = new HID.HID(deviceId);
     scanner.on('data', reader);
 } catch (e) {
     console.log(e);
 }
-
-function reader(data) {
-    console.log(data.toString());
-}
-
-//0268098208
-
-/*
-
-function reader(err, data) {
-
-    var size, id;
-
-    console.log("\n" + data.map(function (v) {return ('00' + v.toString(16)).slice(-2)}).join(','));
-
-*/
-/*    //get 64 bytes
-    if (data[0] != 0) {
-
-        console.log("\n" + data.map(function (v) {return ('00' + v.toString(16)).slice(-2)}).join(','));
-
-        switch (data[0]) {
-            case 1:
-                //Orientation change
-                switch (data[1]) {
-                    case 4:
-                        console.log("-> mir:ror up");
-                        break;
-                    case 5:
-                        console.log("-> mir:ror down");
-                        break;
-                }
-                break;
-            case 2:
-                //RFID
-                switch (data[1]) {
-                    case 1:
-                        console.log("-> RFID in");
-                        break;
-                    case 2:
-                        console.log("-> RFID out");
-                        break;
-                }
-
-                size = data[4];
-                id = (data.splice(0)).splice(5, size);
-                console.log(id.map(function (v) {return ('00' + v.toString(16)).slice(-2)}).join(','));
-                break;
-        }
-    }*//*
-
-
-    scanner.read(reader);
-}*/
